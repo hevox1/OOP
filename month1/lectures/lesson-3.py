010a72e36383586698ecb62772ca0cb6b6f15e0b
hevox1 <hevox1231@gmail.com>
2025-06-17 14:29:53 +0300

    OOP

--- month1/lectures/lesson-3.py
+++ month1/lectures/lesson-3.py
@@ -4,7 +4,7 @@
 class Emirlan:
     def __init__(self, name='Emirlan', age=18):
         self.__name=name
-        self._age=age
+        self.__age=age
     def __str__(self):
         return f'{self.__name}' \
                f'{self._age}'
@@ -15,8 +15,17 @@ class Emirlan:
     def __run(self):
         print(self.__name, 'run')
 
-    def __g(self):
-        pass
+    @property
+    def emirlan(self):
+        return f'{self.__name} {self.__age}'
+    @emirlan.setter
+    def emirlan(self, name, age):
+        self.__name=name
+        self.__age=age
+    # @name.setter
+    # def name(self, name):
+    #     self.__name=name
+
 
 
 e=Emirlan()
@@ -28,6 +37,16 @@ e=Emirlan()
 
 # print(e._Emirlan__name)
 # e._Emirlan__run()
+
+
+amir=Emirlan('Emirlan', 0)
+# amir.set_emirlan('Amir', 12)
+# print(amir.get_emirlan())
+amir.emirlan
+amir.emirlan=
+
+
+
 
 
 
