9d0762630f950ecd70eb3b2d570433473813aadb
hevox1 <hevox1231@gmail.com>
2025-06-22 10:48:27 +0300

    OOP

---
+++ month1/lectures/lesson5.py
@@ -0,0 +1,16 @@
+# 1 встроенный
+# import math
+
+# from math import pi as aldiar
+# print(pi)
+# print(aldiar)
+
+# 2 свои модули
+# import lesson3
+# from lesson3 import Emirlan
+# a=Emirlan('bayel', 12)
+# a.run()
+
+# 3 взаимствованные модули
+# import colorama
+# print(colorama.Back.GREEN)
